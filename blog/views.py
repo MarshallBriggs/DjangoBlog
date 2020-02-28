e7951afae7fcb78ec26cedf4c07aca7a98314c00
Marshall Briggs <marshallbriggs1@gmail.com>
2020-02-28 15:06:08 -0800

    Database commit

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,9 @@
 from django.shortcuts import render
-
-# Dummy info
-posts = [
-    {
-        'author': 'Marshall Briggs',
-        'title': 'Blog Post 1',
-        'content': 'First Post Content',
-        'date_posted': 'February 28, 2020'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second Post Content',
-        'date_posted': 'February 29, 2020'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
