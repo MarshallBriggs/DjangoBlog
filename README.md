5128a7a5d444a8501389a48b579c56f271ba5c4c
Marshall Briggs <marshallbriggs1@gmail.com>
2020-02-28 14:05:48 -0800

    Templates commit

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # DjangoBlog
 This is a Blog based on Django guides online
+https://www.youtube.com/playlist?list=PL-osiE80TeTtoQCKZ03TU5fNfx2UY6U4p
+
 
 Virtualenv is located in "djangoblog_env\Scripts\activate.bat"
 Run that from cmd to enter virtualenv
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+# Dummy info
+posts = [
+    {
+        'author': 'Marshall Briggs',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': 'February 28, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second Post Content',
+        'date_posted': 'February 29, 2020'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
